4e7fe9a79ef36e9f9ffbf491fa7039fab4d66bda
Solpostgit <begemot_off@mail.ru>
2023-10-10 23:04:22 +0300

    Убрал методы, так как они написаны в классе Предок, нет необходимости их дублировать в классе Потомок.

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -2,8 +2,10 @@ public class Bicycle extends Transport {
     public Bicycle(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
-
+/* // методы написаны в классе Предок, нет необходимости их здесь указывать.
     public void updateTyre() {
         System.out.println("Меняем покрышку");
     }
+
+ */
 }
