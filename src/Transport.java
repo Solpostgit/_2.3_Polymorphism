634b038aba724032f9c1d8740b0861a4314c0748
Solpostgit <begemot_off@mail.ru>
2023-10-10 22:55:22 +0300

    Применил тип "Полиморфизма" - "Перегрузка" метода ServiceStation

--- src/Transport.java
+++ src/Transport.java
@@ -11,6 +11,14 @@ public class Transport {
         System.out.println("Меняем покрышку");
     }
 
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
     public String getModelName() {
         return modelName;
     }
