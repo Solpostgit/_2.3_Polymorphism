e57805370009bd832ef722a582277f536f9f7315
Solpostgit <begemot_off@mail.ru>
2023-10-10 06:05:38 +0300

    Add Getter and Setter in the class Bicycle

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,8 +1,24 @@
 public class Bicycle {
-    public String modelName;
-    public int wheelsCount;
+    private String modelName;
+    private int wheelsCount;
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
     }
 }
