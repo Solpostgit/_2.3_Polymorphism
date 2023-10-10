554094d48cc7d398d1c4d9d654b62d8d20958ac6
Solpostgit <begemot_off@mail.ru>
2023-10-10 21:51:47 +0300

    Применил принцип ООП "Наследование", добавил класс предок-Transport.

--- src/Car.java
+++ src/Car.java
@@ -1,14 +1,15 @@
-public class Car {
+public class Car extends Transport {
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
 
-        public String modelName;
-        public int wheelsCount;
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
 
-        public void updateTyre() {
-            System.out.println("Меняем покрышку");
-        }
-
-        public void checkEngine() {
-            System.out.println("Проверяем двигатель");
-        }
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
 }
+
 
