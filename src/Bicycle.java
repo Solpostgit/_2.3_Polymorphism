8a228c2bebf9219cfec6cdc636e76768ed2db2cb
Solpostgit <begemot_off@mail.ru>
2023-10-12 20:22:35 +0300

    удалил интерфейс и класс Transport. Перенёс блок Bicycle наверх, перед Car.

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,11 +1,29 @@
-public class Bicycle extends Transport {
-    public Bicycle(String modelName, int wheelsCount) {
-        super(modelName, wheelsCount);
+public class Bicycle {
+    private String modelName;
+    private int wheelsCount;
+
+    public Bicycle (String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
     }
-/* // методы написаны в классе Предок, нет необходимости их здесь указывать.
+
     public void updateTyre() {
         System.out.println("Меняем покрышку");
     }
 
- */
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
 }
