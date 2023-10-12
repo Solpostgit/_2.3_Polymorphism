faa50bc84146a222f4611e64880fbc848444dab1
Solpostgit <begemot_off@mail.ru>
2023-10-12 20:25:49 +0300

    Создал класс-предок EngineTransport c методом checkEngine(); и наследовал классы Car и Truck.

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,30 @@
-public class Car extends Transport {
-    public Car(String modelName, int wheelsCount) {
-        super(modelName, wheelsCount);
+public class Car extends EngineTransport {
+    private String modelName;
+    private int wheelsCount;
+
+    public Car (String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
     }
 }
 
--- src/Truck.java
+++ src/Truck.java
@@ -1,5 +1,33 @@
-public class Truck extends Transport {
-    public Truck(String modelName, int wheelsCount) {
-        super(modelName, wheelsCount);
+public class Truck extends EngineTransport {
+    private String modelName;
+    private int wheelsCount;
+
+    public Truck (String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
     }
 }
