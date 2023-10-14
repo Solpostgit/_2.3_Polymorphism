5e734f10ac65bef3a79266268a12eddabd4a8711
Solpostgit <begemot_off@mail.ru>
2023-10-14 21:37:42 +0300

    Расширил класс Truck от класса-предка EngineTransport

--- src/Truck.java
+++ src/Truck.java
@@ -1,33 +1,15 @@
 public class Truck extends EngineTransport {
-    private String modelName;
-    private int wheelsCount;
-
     public Truck (String modelName, int wheelsCount) {
-        this.modelName = modelName;
-        this.wheelsCount = wheelsCount;
+       super(modelName, wheelsCount);
     }
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
     }
 
-    public String getModelName() {
-        return modelName;
-    }
-
-    public void setModelName(String modelName) {
-        this.modelName = modelName;
-    }
-
-    public int getWheelsCount() {
-        return wheelsCount;
-    }
-
-    public void setWheelsCount(int wheelsCount) {
-        this.wheelsCount = wheelsCount;
+    @Override
+    public void check() {
+        System.out.println("Началось обслуживание грузовика " + getModelName());
+        super.check();
+        checkTrailer();
     }
 }
