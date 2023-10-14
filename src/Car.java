838629519b8844a1b4f2acf9a80584567aca336d
Solpostgit <begemot_off@mail.ru>
2023-10-14 21:37:14 +0300

    Расширил класс Car от класса-предка EngineTransport

--- src/Car.java
+++ src/Car.java
@@ -1,30 +1,12 @@
 public class Car extends EngineTransport {
-    private String modelName;
-    private int wheelsCount;
-
     public Car (String modelName, int wheelsCount) {
-        this.modelName = modelName;
-        this.wheelsCount = wheelsCount;
+       super(modelName, wheelsCount);
     }
 
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-
-    public String getModelName() {
-        return modelName;
-    }
-
-    public void setModelName(String modelName) {
-        this.modelName = modelName;
-    }
-
-    public int getWheelsCount() {
-        return wheelsCount;
-    }
-
-    public void setWheelsCount(int wheelsCount) {
-        this.wheelsCount = wheelsCount;
+    @Override
+    public void check() {
+        System.out.println("Началось обслуживание легкового автомобиля " + getModelName());
+        super.check();
     }
 }
 
