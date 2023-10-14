fa12b54bdaa008f62b544118e517fda01d39d576
Solpostgit <begemot_off@mail.ru>
2023-10-14 21:40:51 +0300

    Создал объекты классов Bicycle, Car, Truck, ServiceStation.

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         Bicycle bicycle = new Bicycle("bicycle1", 2);
-        Bicycle bicycle2 = new Bicycle("bicycle2", 2);
 
-        Car car = new Car("car1", 4);
-        Car car2 = new Car("car2", 4);
+        Car car = new Car("Audi", 4);
 
-        Truck truck = new Truck("truck1", 6);
-        Truck truck2 = new Truck("truck2", 8);
+        Truck truck = new Truck("ГАЗ", 6);
 
-        ServiceStation station = new ServiceStation();
-        station.check(bicycle);
-        station.check(bicycle2);
-        station.check(car);
-        station.check(car2);
-        station.check(truck);
-        station.check(truck2);
+
+        ServiceStation serviceStation = new ServiceStation();
+
+        serviceStation.check(bicycle);
+
+        serviceStation.check(car);
+
+        serviceStation.check(truck);
+
     }
 }
